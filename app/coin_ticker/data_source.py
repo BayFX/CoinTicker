8f297b0e3ef397d2ec5058bcb6f8722e737d7b75
RevaxZnarf <>
2017-12-29 19:38:26 +0100

    Adds logs

--- app/coin_ticker/data_source.py
+++ app/coin_ticker/data_source.py
@@ -1,5 +1,7 @@
 import requests
 import yaml
+import logging
+
 
 class Trade(object):
     def __init__(self, name, symbol, volume, cost):
@@ -7,6 +9,7 @@ class Trade(object):
         self.symbol = symbol
         self.volume = volume
         self.cost = cost
+
 
 class Data(object):
     def __init__(self, gain, history):
@@ -19,17 +22,23 @@ class Data(object):
     def history(self):
         return self.__history
 
+
 class DataSource(object):
     def __init__(self, config):
+        self._logger = logging.getLogger(__name__)
         self.trades = self.load_trades(config)
 
     def load_trades(self, config):
+        self._logger.info('Loading trades from %s', config)
         stream = open(config, "r")
         config = yaml.load(stream)
         trades_config = config['trades']
         trades = []
         for trade in trades_config:
-            trades.append(Trade(trade['name'], trade['symbol'], trade['volume'], trade['cost']))
+            trades.append(Trade(trade['name'],
+                                trade['symbol'],
+                                trade['volume'],
+                                trade['cost']))
         return trades
 
     def fetch_coinmarketcap_prices(self, prices):
@@ -41,15 +50,16 @@ class DataSource(object):
 
     def calculate_data(self):
         prices = self.fetch_coinmarketcap_prices({})
-        total_gain = 0
+        total_value = 0
+        total_cost = 0
         for trade in self.trades:
             price = prices[trade.symbol]
             value = trade.volume * price
-            profit = value - trade.cost
-            gain = (profit / trade.cost) * 100.0
-            total_gain += gain
-
-        return Data(total_gain, [])
+            total_cost += trade.cost
+            total_value += value
+        total_profit = total_value - total_cost
+        gain = (total_profit / total_cost) * 100.0
+        return Data(gain, [])
 
     def get(self):
         data = self.calculate_data()
--- app/coin_ticker/run.py
+++ app/coin_ticker/run.py
@@ -2,16 +2,19 @@
 
 from time import sleep
 from updater import Updater
+import logging
 import sys
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 config = sys.argv[1]
 UPDATER = Updater(config)
 
 try:
-    print "Starting Coin Ticker"
+    logger.info("Starting Coin Ticker")
     UPDATER.start()
     while True:
         sleep(60)
 except KeyboardInterrupt:
-    print "Stopping Coin Ticker"
+    logger.info("Stopping Coin Ticker")
     UPDATER.stop()
