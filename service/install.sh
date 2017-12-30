1a97be6b6f6c6b8a1d1c425304f8cb3876213458
RevaxZnarf <>
2017-12-30 20:51:31 +0100

    Update ansible installer

--- service/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo cp cointicker.service /lib/systemd/system/cointicker.service
-sudo chmod 644 /lib/systemd/system/cointicker.service
-chmod +x /home/pi/CoinTicker/app/coin_ticker/run.py
-sudo systemctl daemon-reload
-sudo systemctl enable cointicker.service
-sudo systemctl start cointicker.service
