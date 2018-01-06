57ca1a8db40f9cb2e77f397d609337614fd71ecf
Franz Xaver Bayerl <mail@franz-xaver-bayerl.de>
2018-01-06 11:04:20 +0100

    Support stock trades

--- app/coin_ticker/data_source.py
+++ app/coin_ticker/data_source.py
@@ -5,11 +5,12 @@ import datetime
 
 
 class Trade(object):
-    def __init__(self, name, symbol, volume, cost):
+    def __init__(self, name, symbol, volume, cost, trade_type):
         self.name = name
         self.symbol = symbol
         self.volume = volume
         self.cost = cost
+        self.trade_type = trade_type
 
 
 class Data(object):
@@ -35,18 +36,27 @@ class DataSource(object):
         self._logger.info('Loading trades from %s', config)
         stream = open(config, "r")
         config = yaml.load(stream)
-        trades_config = config['trades']
         trades = []
-        for trade in trades_config:
-            trades.append(Trade(trade['name'],
-                                trade['symbol'],
-                                trade['volume'],
-                                trade['cost']))
+        if 'coins' in config['trades']:
+            for trade in config['trades']['coins']:
+                trades.append(Trade(trade['name'],
+                                    trade['symbol'],
+                                    trade['volume'],
+                                    trade['cost'],
+                                    'coin'))
+        if 'stocks' in config['trades']:
+            for trade in config['trades']['stocks']:
+                trades.append(Trade(trade['name'],
+                                    trade['symbol'],
+                                    trade['volume'],
+                                    trade['cost'],
+                                    'stock'))
         return trades
 
     def fetch_coin_prices(self):
         prices = {}
-        required_symbols = map(lambda t: t.symbol, self.trades)
+        coin_trades = filter(lambda t: t.trade_type == 'coin', self.trades)
+        required_symbols = map(lambda t: t.symbol, coin_trades)
         for symbol in required_symbols:
             try:
                 url = "https://api.cryptonator.com/api/ticker/%s-usd" % symbol
@@ -61,11 +71,33 @@ class DataSource(object):
                 self._logger.exception('Unable to fetch symbol %s', symbol)
         return prices
 
+    def fetch_stock_prices(self):
+        prices = {}
+        stock_trades = filter(lambda t: t.trade_type == 'stock', self.trades)
+        required_symbols = map(lambda t: t.symbol, stock_trades)
+        for symbol in required_symbols:
+            try:
+                url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&outputsize=compact&datatype=json&interval=1min&apikey=24IXEF00K07P6PGQ" % symbol
+                response = requests.get(url)
+                response.raise_for_status()
+                responseJson = response.json()
+                if "Time Series (1min)" in responseJson:
+                    timeseries = responseJson["Time Series (1min)"]
+                    first = timeseries[timeseries.keys()[0]]
+                    prices[symbol] = float(first["1. open"])
+                else:
+                    self._logger.error("Unable to fetch data for symbol %s", symbol)
+            except requests.exceptions.RequestException:
+                self._logger.exception('Unable to fetch symbol %s', symbol)
+        return prices
+
     def calculate_data(self):
-        prices = self.fetch_coin_prices()
+        coin_prices = self.fetch_coin_prices()
+        stock_prices = self.fetch_stock_prices()
         total_value = 0
         total_cost = 0
         for trade in self.trades:
+            prices = coin_prices if trade.trade_type == 'coin' else stock_prices
             if (trade.symbol in prices):
                 price = prices[trade.symbol]
                 value = trade.volume * price
